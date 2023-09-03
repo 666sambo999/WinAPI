e610eb76eb9c3e56cdbf3f6687204cb396a3e86f
Koros_zl65@top-academy.ru <Koros_zl65@top-academy.ru>
2023-09-04 01:28:59 +0500

    WinP

--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -1,5 +1,5 @@
 #include <Windows.h>
-#include"resource.h"
+#include "resource.h"
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 //HWND -  окно(Handler to Windows - обработчик окна)
@@ -14,23 +14,41 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	//	NULL,			// родительское окно, если его нет - параметр  NULL
 	//	"Hello World!", 
 	//	"Info", 
-	//	MB_YESNOCANCEL | MB_ICONERROR | MB_HELP| MB_DEFBUTTON3| MB_APPLMODAL
+	//	MB_YESNOCANCEL | MB_ICONERROR | MB_HELP| MB_DEFBUTTON3| MB_APPLMODAL 
 	//);
 	//return 0;
 	// модальное окно - это окноБ которое блокирует родительское окно;
-
+	// каждый элемент интерфейса является окном 
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
 	return 0;
+
 }
 // процедура окна 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-	case WM_INITDIALOG: break;
+	case WM_INITDIALOG: 
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		SetFocus(hEditLogin); // выделяем окно для печати 
+	}
+	break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON_COPY: 
+		{
+			CONST INT SIZE = 256;
+			CHAR  sz_buffer[SIZE] = {};
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		break; 
 		case IDOK: MessageBox(hwnd, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL: EndDialog(hwnd, 0);
 		}
