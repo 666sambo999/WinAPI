dc52a017c5aad2f8ab82a16a5d5bef0c9d867afe
Koros_zl65@top-academy.ru <Koros_zl65@top-academy.ru>
2023-10-13 00:32:19 +0500

    Clock2

--- Clock/Form1.Designer.cs
+++ Clock/Form1.Designer.cs
@@ -33,17 +33,22 @@
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.cbShowDate = new System.Windows.Forms.CheckBox();
+            this.btnHideControl = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnDateCalculator = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Vladimir Script", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(31, 28);
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Vivaldi", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 58);
+            this.label1.Size = new System.Drawing.Size(86, 44);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
+            this.label1.MouseHover += new System.EventHandler(this.label1_MouseHover);
             // 
             // timer1
             // 
@@ -54,24 +59,65 @@
             // 
             this.cbShowDate.AutoSize = true;
             this.cbShowDate.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbShowDate.Location = new System.Drawing.Point(51, 105);
+            this.cbShowDate.Location = new System.Drawing.Point(282, 28);
             this.cbShowDate.Name = "cbShowDate";
             this.cbShowDate.Size = new System.Drawing.Size(150, 31);
             this.cbShowDate.TabIndex = 1;
             this.cbShowDate.Text = "Show date";
             this.cbShowDate.UseVisualStyleBackColor = true;
+            this.cbShowDate.CheckedChanged += new System.EventHandler(this.cbShowDate_CheckedChanged);
+            // 
+            // btnHideControl
+            // 
+            this.btnHideControl.Font = new System.Drawing.Font("Snap ITC", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHideControl.Location = new System.Drawing.Point(41, 164);
+            this.btnHideControl.Name = "btnHideControl";
+            this.btnHideControl.Size = new System.Drawing.Size(292, 46);
+            this.btnHideControl.TabIndex = 2;
+            this.btnHideControl.Text = "Hide controls";
+            this.btnHideControl.UseVisualStyleBackColor = true;
+            this.btnHideControl.Click += new System.EventHandler(this.btnHideControl_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Snap ITC", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(41, 269);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(292, 46);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnDateCalculator
+            // 
+            this.btnDateCalculator.Font = new System.Drawing.Font("Snap ITC", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDateCalculator.Location = new System.Drawing.Point(41, 217);
+            this.btnDateCalculator.Name = "btnDateCalculator";
+            this.btnDateCalculator.Size = new System.Drawing.Size(292, 46);
+            this.btnDateCalculator.TabIndex = 4;
+            this.btnDateCalculator.Text = "Date calculator";
+            this.btnDateCalculator.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(541, 298);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(440, 322);
+            this.Controls.Add(this.btnDateCalculator);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnHideControl);
             this.Controls.Add(this.cbShowDate);
             this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "MyClock";
-            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.TransparencyKey = System.Drawing.SystemColors.ControlLight;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -83,6 +129,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.CheckBox cbShowDate;
+        private System.Windows.Forms.Button btnHideControl;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnDateCalculator;
     }
 }
 
--- Clock/Form1.cs
+++ Clock/Form1.cs
@@ -15,16 +15,58 @@ namespace Clock
         public Form1()
         {
             InitializeComponent();
+            this.Location = new System.Drawing.Point
+            (
+                System.Windows.Forms.Screen.PrimaryScreen.Bounds.Right - this.Width - 50,
+                System.Windows.Forms.Screen.PrimaryScreen.Bounds.Top + 250
+            );
+            
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Text = DateTime.Now.ToString("HH:mm:ss");
+            label1.Text = DateTime.Now.ToString("HH:mm:ss tt\n");
+            //label1.Text = DateTime.Now.ToString("HH:mm:ss tt"); // AM PM
+            if(cbShowDate.Checked)
+            {
+                label1.Text += DateTime.Now.ToString("dd:MM:yyyy");
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void cbShowDate_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("dd:MM:yyyy");
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnHideControl_Click(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            //this.BackgroundImage = BackgroundImage.Dispose();
+            this.ShowInTaskbar = false;
+            cbShowDate.Visible = false;
+            btnHideControl.Visible = false;
+            btnDateCalculator.Visible = false;
+            btnClose.Visible = false;
+        }
+
+        private void label1_MouseHover(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.ShowInTaskbar = true;
+            cbShowDate.Visible = true;
+            btnHideControl.Visible = true;
+            btnDateCalculator.Visible = true;
+            btnClose.Visible = true;
         }
     }
 }
